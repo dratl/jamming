c3eaa68f1e97795c6a91b6323e91c04fdb314eb9
Xico Trujano <xtrujano@mac.com>
2025-03-13 21:04:40 -0600

    Static Components Faux Data

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
+function Playlist() {
+  return (
+    <div className="Playlist">
+      <h2>Playlist</h2>
+    </div>
+  );
+}
+
 export default Playlist;
--- src/components/Tracklist.js
+++ src/components/Tracklist.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
+function Tracklist() {
+  return (
+    <div className="Tracklist">
+      <h2>Tracklist</h2>
+    </div>
+  );
+}
+
 export default Tracklist;
